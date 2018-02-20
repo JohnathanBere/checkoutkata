1bc00e0e2eb521c9be285186196e09da33979b56
JohnathanBere <johnbere@live.co.uk>
2018-02-20 14:50:26 +0000

    First commit

---
+++ KataWebTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace KataWebTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void ItShouldTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
